a575c86c880ac4c8ab2dc8d3a18818e68c768148
Javier La Cruz <idsistemas15@gmail.com>
2025-03-07 11:53:32 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,18 @@
-import { useState, useEffect, JSX } from 'react'
-import './App.css'
+import Button from 'remoteApp/Button'
+
+const appTitle = import.meta.env.VITE_VAR1
 
 function App() {
-  const [remotes, setRemotes] = useState<JSX.Element[]>([])
-
-  useEffect(() => {
-    const loadRemoteComponents = async () => {
-      try {
-        // Cargar la configuración de los remotes
-        const response = await fetch('/remotes.json')
-        const { remotes: remoteConfigs } = await response.json()
-
-        const remoteComponents = await Promise.all(
-          remoteConfigs.map(async (remote: any) => {
-            // Verificar si el contenedor del remote ya está cargado
-            if (!window[remote.scope]) {
-              // Si no está cargado, cargar el remote dinámicamente
-              await loadRemoteEntry(remote.url)
-            }
-
-            // Acceder al contenedor del remote
-            const container = window[remote.scope]
-
-            // Obtener el módulo expuesto
-            const Module = await container.get(remote.module)
-
-            // Renderizar el componente
-            const Component = Module().default
-            return <Component key={remote.name} />
-          }),
-        )
-
-        setRemotes(remoteComponents)
-      } catch (error) {
-        console.error('Error loading remote components:', error)
-      }
-    }
-
-    loadRemoteComponents()
-  }, [])
-
   return (
     <div>
       <div></div>
-      <h1>Host</h1>
-      <div className="card">{remotes}</div>
+      <h1>HOST</h1>
+      <h1>{appTitle}</h1>
+      <div className="card">
+        <Button />
+      </div>
     </div>
   )
-}
-
-// Función para cargar dinámicamente un remote entry
-const loadRemoteEntry = (url: string) => {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script')
-    script.src = url
-    script.onload = resolve
-    script.onerror = reject
-    document.head.appendChild(script)
-  })
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import federation from '@originjs/vite-plugin-federation';
-import { loadRemotes } from './src/services/remoteService';
+import { defineConfig } from 'vite'
+import federation from '@originjs/vite-plugin-federation'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(async () => {
-  const federationConfig = await loadRemotes();
-
-  return {
-    plugins: [
-      react(),
-      federation(federationConfig),
-    ],
-    build: {
-      modulePreload: false,
-      target: 'esnext',
-      minify: false,
-      cssCodeSplit: false,
-    },
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'app',
+      remotes: {
+        remoteApp: 'http://localhost:5001/assets/remoteEntry.js',
+      },
+      shared: ['react', 'react-dom']
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
+})
