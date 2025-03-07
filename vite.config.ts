85fe4947db4749a500e0dbc703c92a8c2eb77858
Javier La Cruz <idsistemas15@gmail.com>
2025-03-07 14:25:49 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,23 @@
-import { defineConfig } from 'vite';
-import federation from '@originjs/vite-plugin-federation';
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from 'vite'
+import federation from '@originjs/vite-plugin-federation'
+import react from '@vitejs/plugin-react'
 
-// Leer los remotes desde un archivo externo
-const remotesPath = path.resolve(__dirname, 'remotes.json');
-let remotes = {};
-
-if (fs.existsSync(remotesPath)) {
-  try {
-    const remotesData = JSON.parse(fs.readFileSync(remotesPath, 'utf-8'));
-    console.log('Contenido de remotes.json:', remotesData);
-
-    if (Array.isArray(remotesData)) {
-      remotes = remotesData.reduce((acc, remote) => {
-        acc[remote.name] = `${remote.url}/assets/remoteEntry.js`;
-        return acc;
-      }, {});
-    } else {
-      console.error('Error: remotes.json no contiene un array válido.');
-    }
-  } catch (error) {
-    console.error('Error leyendo remotes.json:', error);
-  }
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
     federation({
-      name: 'host-app',
-      remotes,
+      name: 'app',
+      remotes: {
+        remoteApp: 'http://localhost:5001/assets/remoteEntry.js',
+      },
       shared: ['react', 'react-dom']
     })
   ],
-  server: {
-    port: 5000,
-  },
   build: {
+    modulePreload: false,
     target: 'esnext',
     minify: false,
+    cssCodeSplit: false
   }
-});
+})
